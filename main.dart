1df80a39d29a2851ada933764f749a9bb61f8347
Kawamatsuu <146920787+XyrelTenz@users.noreply.github.com>
2025-09-23 14:33:40 +0800

    New Algo

--- main.dart
+++ main.dart
@@ -1,40 +1,32 @@
-// import 'MiniProjects/MiniBankingSystem/main.dart';
 import "dart:io";
 
+int binarySearch(List<int> arr, int x) {
+  int low = 0;
+  int high = arr.length - 1;
 
-int binarySearch(int arr, int x){
-    int low = 0;
-    int high = arr.length - 1;
+  while (low <= high) {
+    int mid = low + (high - low) ~/ 2;
 
-    while(low <= high){
-      int mid = low + (high - low ) / 2;
-        
-      if(arr[mid] ==x){
-        return mid;
-      }
-
-      if(arr[mid] < x){
-        low = mid + 1;
-      }
-          
-    return high = mid - 1;
+    if (arr[mid] == x) {
+      return mid; // found
+    } else if (arr[mid] < x) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
   }
+
+  return -1; // not found
 }
 
 void main() {
-
-  List<int> arr = {
-      1,1,3,4,5,6
-    };
-  int x =10;
+  List<int> arr = [1, 1, 3, 4, 5, 6, 10];
+  int x = 10;
   int result = binarySearch(arr, x);
 
-  if(result == -1){
-      stdcout.write("Element is not present in array");
-    }else{
-        stdcout.write("Element is present at ${result}");
-      }
-
-  // BankingSystem();
-
+  if (result == -1) {
+    print("Element is not present in array");
+  } else {
+    print("Element is present at $result");
+  }
 }
